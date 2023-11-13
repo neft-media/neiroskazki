5869f46187eb77a899cd1f6b7215c3bf6e7d81c9
Andrey <andrey2455@gmail.com>
2023-11-13 11:09:28 +0700

    Почти готова первая сказка

--- src/components/lang-selector/lang-selector.jsx
+++ src/components/lang-selector/lang-selector.jsx
@@ -2,12 +2,12 @@ import clsx from "clsx";
 
 import styles from "./lang-selector.module.css";
 
-function LangSelector({lang1, lang2, active, toggleLang}) {
+function LangSelector({lang1, lang2, rus, toggleLang}) {
 
   return (
     <div className={clsx(styles.lang_wrapper)}>
-      <button className={clsx(active==lang1 && styles.active, styles.lang, styles.lang1)} onClick={toggleLang}>{lang1}</button>
-      <button className={clsx(active==lang2 && styles.active, styles.lang, styles.lang2)} onClick={toggleLang}>{lang2}</button>
+      <button className={clsx(rus && styles.active, styles.lang, styles.lang1)} onClick={toggleLang}>{lang1}</button>
+      <button className={clsx(!rus && styles.active, styles.lang, styles.lang2)} onClick={toggleLang}>{lang2}</button>
     </div>
   );
 }
--- src/pages/tales/khanty/khanty.jsx
+++ src/pages/tales/khanty/khanty.jsx
@@ -2,19 +2,31 @@ import React from 'react';
 import clsx from 'clsx';
 
 import { useDisclosure } from '@mantine/hooks';
-import { Drawer, Button } from '@mantine/core';
 
+import { Drawer} from '@mantine/core';
 import { Burger } from '@mantine/core';
+
+import { motion } from "framer-motion"
 
 import LangSelector from '../../../components/lang-selector/lang-selector';
 
 import styles from "./khanty.module.css";
-import patternKhanty from '../../../images/pattern-khanty.svg';
 
-import { tale_1_rus, tale_1_khanty } from "../../../tales/khanty";
+// Картинки
+import patternKhanty from '../../../images/khanty/pattern-khanty.svg';
+import tale1Image1Hor from "../../../images/khanty/tale1/khant_1_1_h.jpg";
+import tale1Image1Vert from "../../../images/khanty/tale1/khant_1_1_v.jpg";
+import tale1Image2Hor from "../../../images/khanty/tale1/khant_1_2_h.jpg";
+import tale1Image2Vert from "../../../images/khanty/tale1/khant_1_2_v.jpg";
+import tale1Image3Hor from "../../../images/khanty/tale1/khant_1_3_h.jpg";
+import tale1Image3Vert from "../../../images/khanty/tale1/khant_1_3_v.jpg";
+
+import { tale_1_rus, tale_1_khanty } from "../../../tales/khanty/khanty-1";
 
 
 function Khanty() {
+
+  const imageBreakpoint = "700px";
 
   const [menuOpened, menuHandlers] = useDisclosure(false);
 
@@ -23,28 +35,38 @@ function Khanty() {
   const lang1 = "РУС";
   const lang2 = "ХАНТ";
 
-  const [tale1Lang, setTale1Lang] = React.useState(lang1);
+  const [tale1IsRus, tale1LangHandler] = useDisclosure(localStorage.getItem("khantTale1IsRus") === "true");
 
-  const toggleTale1Lang = () => {
-    setTale1Lang(tale1Lang === lang1 ? lang2 : lang1);
-  };
+  React.useEffect(() => {
+    localStorage.setItem('khantTale1IsRus', tale1IsRus);
+  }, [tale1IsRus]);
+
+
 
   let tale1 = tale_1_rus;
 
-  if (tale1Lang === lang2) {
+  if (!tale1IsRus) {
     tale1 = tale_1_khanty;
+  }
+
+  // Animations
+  const animations = {
+    initial: {y: 100, opacity: 0},
+    whileInView: {y: 0, opacity: 1},
+    viewport: {once: true},
+    transition: {type: "spring", stiffness: 100}
   }
 
   return (
     <>
       <div className={clsx("page")}>
 
-        {/* <Button onClick={open}>Open Drawer</Button> */}
-        <Burger className={clsx("burger-button")} opened={menuOpened} onClick={menuHandlers.toggle} aria-label="Toggle navigation" />
 
+
+        {/* Меню */}
+        <Burger className={clsx("burger-button")} opened={menuOpened} onClick={menuHandlers.toggle} aria-label="Toggle navigation" />
         <Drawer
           position="right"
-          // withCloseButton={false}
           opened={menuOpened}
           onClose={menuHandlers.toggle}
           transitionProps={{ transition: 'slide-left', duration: 250, timingFunction: "ease" }}
@@ -52,7 +74,10 @@ function Khanty() {
           <p>Hello</p>
         </Drawer>
 
-        {/* <p className='main_title'>Нейросказки</p> */}
+
+
+        {/* Заголовок и вводный абзац */}
+
         <h1 className={clsx(styles.clr_1, "mt-5")}>Хантыйские сказки</h1>
         <p className={clsx("intro_text", "mt-4")}>
         Название Ханты-Мансийского автономного округа составлено из&nbsp;названий двух коренных народов
@@ -67,11 +92,94 @@ function Khanty() {
         </div>
 
 
-        <LangSelector lang1={lang1} lang2={lang2} active={tale1Lang} toggleLang={toggleTale1Lang}/>
+
+        {/*-------------- Сказка 1 ----------------*/}
+
+        <motion.div {...animations}>
+          <LangSelector lang1={lang1} lang2={lang2} rus={tale1IsRus} toggleLang={tale1LangHandler.toggle}/>
+        </motion.div>
 
         <div className={clsx("tale")}>
-          <h2 className={clsx(styles.clr_1, "mt-5")}>{tale1.title}</h2>
-          <p className={clsx("mt-2")}>{tale1.content[0]}</p>
+
+          <motion.h2 {...animations} className={clsx(styles.clr_1, "mt-5")}>{tale1.title}</motion.h2>
+          <motion.p {...animations} className={clsx("mt-2")}>{tale1.content.p0}</motion.p>
+
+          <motion.div {...animations}>
+            <picture className="image">
+              <source
+                media={`(max-width: ${imageBreakpoint})`}
+                srcSet={`${tale1Image1Vert} 1000w`}
+              />
+              <source
+                media={`(min-width: ${imageBreakpoint})`}
+                srcSet={`${tale1Image1Hor} 1500w`}
+              />
+              <img src={tale1Image1Hor} alt="Картинка с копающим котом" />
+            </picture>
+          </motion.div>
+
+          <motion.p {...animations}>{tale1.content.p1}</motion.p>
+
+          <motion.p {...animations}>{tale1.content.p2}</motion.p>
+
+          <motion.p {...animations}>{tale1.content.p3}</motion.p>
+
+          <motion.p {...animations} className={clsx("big_text")}>{tale1.content.p4}</motion.p>
+
+          <motion.p {...animations}>{tale1.content.p5}</motion.p>
+
+          <motion.p {...animations}>{tale1.content.p6}</motion.p>
+
+          <motion.p {...animations}>{tale1.content.p7}</motion.p>
+
+          <motion.p {...animations}>{tale1.content.p8}</motion.p>
+
+          <motion.div {...animations}>
+            <picture className="image">
+              <source
+                media={`(max-width: ${imageBreakpoint})`}
+                srcSet={`${tale1Image2Vert} 1000w`}
+              />
+              <source
+                media={`(min-width: ${imageBreakpoint})`}
+                srcSet={`${tale1Image2Hor} 1500w`}
+              />
+              <img src={tale1Image2Hor} alt="Картинка с белкой и медведем" />
+            </picture>
+          </motion.div>
+
+          <motion.p {...animations}>{tale1.content.p9}</motion.p>
+
+          <motion.p {...animations}>{tale1.content.p10}</motion.p>
+
+          <motion.p {...animations}>{tale1.content.p11}</motion.p>
+
+          <motion.p {...animations} className={clsx("big_text")}>{tale1.content.p12}</motion.p>
+
+          <motion.p {...animations}>{tale1.content.p13}</motion.p>
+
+          <motion.p {...animations}>{tale1.content.p14}</motion.p>
+
+          <motion.p {...animations}>{tale1.content.p15}</motion.p>
+
+          <motion.div {...animations}>
+            <picture className="image">
+              <source
+                media={`(max-width: ${imageBreakpoint})`}
+                srcSet={`${tale1Image3Vert} 1000w`}
+              />
+              <source
+                media={`(min-width: ${imageBreakpoint})`}
+                srcSet={`${tale1Image3Hor} 1500w`}
+              />
+              <img src={tale1Image3Hor} alt="Картинка с пирующими белкой и котом" />
+            </picture>
+          </motion.div>
+
+          <div className={clsx("pattern_wrapper")}>
+            <div style={{ backgroundImage: `url(${patternKhanty})` }} className={clsx("pattern")}></div>
+          </div>
+
         </div>
       </div>
     </>
