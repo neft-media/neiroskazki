d21cafa856e795db18b9febc76e2d055aac2377c
Andrey <andrey2455@gmail.com>
2023-11-22 17:35:58 +0700

    Удалил лишние библиотеки

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,30 +3,22 @@
 import { Routes, Route } from "react-router-dom";
 import { MantineProvider } from '@mantine/core';
 
-import { Scrollbars } from 'rc-scrollbars';
-
 import Homepage from "../../pages/homepage/homepage"
 import Khanty from "../../pages/tales/khanty/khanty";
 import Notfoundpage from "../../pages/notfoundpage/notfoundpage";
 
-// import { OverlayScrollbars } from 'overlayscrollbars';
-
 import '@mantine/core/styles.css';
-// import 'overlayscrollbars/overlayscrollbars.css';
 
-// const osInstance = OverlayScrollbars(document.body, {});
 
 function App() {
   return (
     <>
       <MantineProvider>
-        {/* <Scrollbars style={{ height: "100dvh" }}> */}
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/tales/khanty" element={<Khanty />} />
-            <Route path="*" element={<Notfoundpage />} />
-          </Routes>
-        {/* </Scrollbars> */}
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/tales/khanty" element={<Khanty />} />
+          <Route path="*" element={<Notfoundpage />} />
+        </Routes>
       </MantineProvider>
     </>
   );
